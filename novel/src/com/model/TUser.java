b6113e1c791f34fe662abc8dfc9c2318675cae4e
ouyang <779471275@qq.com>
2019-04-27 16:12:09 +0800

    1.刪除user表中無意義字段userone； 2.刪除userone冗餘代碼。

--- novel/src/com/model/TUser.java
+++ novel/src/com/model/TUser.java
@@ -41,29 +41,7 @@ public class TUser implements java.io.Serializable
 	
 	private String userDel;
 	
-	private String userOne1;
 
-	private String userOne2;
-
-	private String userOne3;
-
-	private String userOne4;
-
-	private String userOne5;
-
-	private Integer userOne6;
-
-	private Integer userOne7;
-
-	private Integer userOne8;
-
-	private Date userOne9;
-
-	private Date userOne10;
-
-	private Long userOne11;
-
-	private Long userOne12;
 
 	// Constructors
 
@@ -76,11 +54,8 @@ public class TUser implements java.io.Serializable
 	public TUser(String userName, String userPw, int userType,
 			String userRealname, String userAddress, String userSex,
 			String userTel, String userEmail, String userQq, String userMan,
-			String userAge, String userBirthday, String userXueli,
-			String userOne1, String userOne2, String userOne3, String userOne4,
-			String userOne5, Integer userOne6, Integer userOne7,
-			Integer userOne8, Date userOne9, Date userOne10, Long userOne11,
-			Long userOne12)
+			String userAge, String userBirthday, String userXueli
+			)
 	{
 		this.userName = userName;
 		this.userPw = userPw;
@@ -95,18 +70,7 @@ public class TUser implements java.io.Serializable
 		this.userAge = userAge;
 		this.userBirthday = userBirthday;
 		this.userXueli = userXueli;
-		this.userOne1 = userOne1;
-		this.userOne2 = userOne2;
-		this.userOne3 = userOne3;
-		this.userOne4 = userOne4;
-		this.userOne5 = userOne5;
-		this.userOne6 = userOne6;
-		this.userOne7 = userOne7;
-		this.userOne8 = userOne8;
-		this.userOne9 = userOne9;
-		this.userOne10 = userOne10;
-		this.userOne11 = userOne11;
-		this.userOne12 = userOne12;
+
 	}
 
 	// Property accessors
@@ -263,124 +227,6 @@ public class TUser implements java.io.Serializable
 		this.userXueli = userXueli;
 	}
 
-	public String getUserOne1()
-	{
-		return this.userOne1;
-	}
 
-	public void setUserOne1(String userOne1)
-	{
-		this.userOne1 = userOne1;
-	}
-
-	public String getUserOne2()
-	{
-		return this.userOne2;
-	}
-
-	public void setUserOne2(String userOne2)
-	{
-		this.userOne2 = userOne2;
-	}
-
-	public String getUserOne3()
-	{
-		return this.userOne3;
-	}
-
-	public void setUserOne3(String userOne3)
-	{
-		this.userOne3 = userOne3;
-	}
-
-	public String getUserOne4()
-	{
-		return this.userOne4;
-	}
-
-	public void setUserOne4(String userOne4)
-	{
-		this.userOne4 = userOne4;
-	}
-
-	public String getUserOne5()
-	{
-		return this.userOne5;
-	}
-
-	public void setUserOne5(String userOne5)
-	{
-		this.userOne5 = userOne5;
-	}
-
-	public Integer getUserOne6()
-	{
-		return this.userOne6;
-	}
-
-	public void setUserOne6(Integer userOne6)
-	{
-		this.userOne6 = userOne6;
-	}
-
-	public Integer getUserOne7()
-	{
-		return this.userOne7;
-	}
-
-	public void setUserOne7(Integer userOne7)
-	{
-		this.userOne7 = userOne7;
-	}
-
-	public Integer getUserOne8()
-	{
-		return this.userOne8;
-	}
-
-	public void setUserOne8(Integer userOne8)
-	{
-		this.userOne8 = userOne8;
-	}
-
-	public Date getUserOne9()
-	{
-		return this.userOne9;
-	}
-
-	public void setUserOne9(Date userOne9)
-	{
-		this.userOne9 = userOne9;
-	}
-
-	public Date getUserOne10()
-	{
-		return this.userOne10;
-	}
-
-	public void setUserOne10(Date userOne10)
-	{
-		this.userOne10 = userOne10;
-	}
-
-	public Long getUserOne11()
-	{
-		return this.userOne11;
-	}
-
-	public void setUserOne11(Long userOne11)
-	{
-		this.userOne11 = userOne11;
-	}
-
-	public Long getUserOne12()
-	{
-		return this.userOne12;
-	}
-
-	public void setUserOne12(Long userOne12)
-	{
-		this.userOne12 = userOne12;
-	}
 
 }
